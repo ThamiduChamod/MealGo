b6c84f2a5dadf2a31f6220ccf609060d27dd35f8
ThamiduChamod <thamiduchamod748@gmail.com>
2026-01-26 17:12:36 +0530

    create meal go project

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../global.css'
+import { View, Text, ActivityIndicator } from 'react-native'
+import { Redirect } from 'expo-router'
+
+export default function index() {
+  const { user, loading } = useAuth()
+  if (loading) {
+    return (
+      <View className="flex-1 justify-center items-center bg-gray-50">
+        <ActivityIndicator size="large" color="#4ade80" />
+      </View>
+    )
+  }
+
+  if (user) {
+    return <Redirect href="/home" />
+  } else {
+    return <Redirect href="/login" />
+  }
+}
