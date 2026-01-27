f3eb8636e9184f1bca925c54d82f8e086c00bb92
ThamiduChamod <thamiduchamod748@gmail.com>
2026-01-27 15:35:31 +0530

    update firebase service

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD6p_WYxNBbCM1CLCVQbTewRtFvswTHt6A",
