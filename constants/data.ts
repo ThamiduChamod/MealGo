6959de77895b788546deb3c3b6c30a1658e703bf
ThamiduChamod <thamiduchamod748@gmail.com>
2026-01-30 23:06:51 +0530

    create new constants

---
+++ constants/data.ts
@@ -0,0 +1,30 @@
+export const FOOD_DATA = [
+    {
+      id: '1',
+      name: 'Classic Beef',
+      price: '1,250',
+      description: 'Juicy beef patty with melted cheese and secret sauce.',
+      rating: '4.8 (120+)',
+      image: require('@/assets/images/b2.png'),
+      ingredients: [
+      { id: 1, name: 'Beef', icon: '🥩' },
+      { id: 2, name: 'Cheese', icon: '🧀' },
+      { id: 3, name: 'Bun', icon: '🍞' },
+      { id: 4, name: 'Onion', icon: '🧅' },
+    ],
+    },
+    {
+      id: '2',
+      name: 'Spicy Zinger',
+      price: '1,100',
+      description: 'Extra crispy chicken with spicy mayo and lettuce.',
+      rating: '4.5 (90+)',
+      image: require('@/assets/images/b2.png'),
+      ingredients: [
+      { id: 1, name: 'Chicken', icon: '🍗' },
+      { id: 2, name: 'Mayo', icon: '🍶' },
+      { id: 3, name: 'Lettuce', icon: '🥬' },
+      { id: 4, name: 'Bun', icon: '🍞' },
+    ],
+    },
+  ];
