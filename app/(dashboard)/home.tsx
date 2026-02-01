9d3f3aeff61b889a9455d48da2917143f46fa29f
ThamiduChamod <thamiduchamod748@gmail.com>
2026-02-02 00:33:43 +0530

    update all

--- app/(dashboard)/home.tsx
+++ app/(dashboard)/home.tsx
@@ -1,30 +1,34 @@
-import React, { useState } from 'react';
-import { View, Text, FlatList, StatusBar, ImageBackground, StyleSheet  } from 'react-native';
-import { KeyboardAvoidingView, Platform } from 'react-native';
-import FoodCardMini from '@/components/foodCardComponents';
-import AutoScrollBanner from '@/components/bannerComponent';
-import { useRouter } from 'expo-router';
-import { FOOD_DATA } from '@/constants/data';
+import AutoScrollBanner from '@/components/bannerComponent'
+import FoodCardMini from '@/components/foodCardComponents'
+import { FOOD_DATA } from '@/constants/data'
+import { Ionicons } from '@expo/vector-icons'
+import { useRouter } from 'expo-router'
+import React, { useState } from 'react'
+import { View, Text, ImageBackground, StyleSheet, KeyboardAvoidingView, Platform, StatusBar, TouchableOpacity, FlatList } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 
-export default function Home() {
+const home = () => {
+  const [activeCat, setActiveCat] = useState(1);
   const router = useRouter();
-  const [selectedFood, setSelectedFood] = useState(null);
-  const [modalOpen, setModalOpen] = useState(false);
 
-  
   const openOrder = (item: any) => {
-    console.log('Opening order for item:', item.name);
-    router.push(`/details/${item.id}?id=${item.id}`);
+    router.push(`/details/${item.id}`);
   };
 
 
+  console.log(activeCat);
 
- 
-
+  const CATEGORIES = [
+    { id: 1, name: "All", icon: "grid-outline" },
+    { id: 2, name: "Burgers", icon: "fast-food-outline" },
+    { id: 3, name: "Buns", icon: "aperture-outline" },
+    { id: 4, name: "Drinks", icon: "wine-outline" },
+    { id: 5, name: "Snacks", icon: "restaurant-outline" },
+  ];
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={{ flex: 1 }}
+            style={{ flex: 1 }}
     >
       <ImageBackground
         source={require("@/assets/images/background.jpg")}
@@ -32,37 +36,74 @@ export default function Home() {
         resizeMode="repeat"
         imageStyle={{ opacity: 0.25 }}
       >
-        <StatusBar  />
+      <StatusBar />
+        <View
+          style={{
+            ...StyleSheet.absoluteFillObject,
+            backgroundColor: "rgba(255,255,255,0.7)",
+          }}
+        />
 
-        {/* --- OVERLAY --- */}
-        <View style={{ ...StyleSheet.absoluteFillObject, backgroundColor: 'rgba(255,255,255,0.7)' }} />
-
-        {/* --- MAIN CONTENT --- */}
         <View style={{ flex: 1, padding: 16, paddingTop: 32 }}>
-          {/* HEADER */}
-          <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 16 }}>Hello, User!</Text>
+          {/* header */}
+          <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 16 }}>
+            Hello, User!
+          </Text>
 
-          {/* SEARCH BAR (You can convert to a separate component) */}
-          {/* ... */}
+          <AutoScrollBanner />
 
-          {/* --- FLATLIST GRID + Banner --- */}
-          <FlatList
-            data={FOOD_DATA}
-            keyExtractor={(item) => item.id}
-            numColumns={2}
-            columnWrapperStyle={{ justifyContent: 'space-between' }}
-            contentContainerStyle={{ paddingBottom: 120 }}
-            renderItem={({ item }) => <FoodCardMini item={item} onOpen={() => openOrder(item)} />}
-            
-             ListHeaderComponent={
-              <>
-                <AutoScrollBanner />
-                <Text style={{ fontSize: 22, fontWeight: 'bold', marginVertical: 10 }}>Popular Foods</Text>
-              </>
+          <View>
+            <ScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{ paddingHorizontal: 4, paddingVertical: 8 }}
+            >
+              {/* Categories will go here */}
+              {CATEGORIES.map((category) => (
+                <TouchableOpacity
+                  key={category.id} 
+                  onPress={() => setActiveCat(category.id)}
+
+                  className='flex-row items-center mr-4 px-6 py-4 rounded-[25px] bg-black'
+                >
+                  <Ionicons
+                    name={category.icon as any}
+                    size={20}
+                    color={activeCat === category.id ? "#FF6347" :"white"}
+                  />
+                  <Text
+                    className={`ml-2 ${
+                      activeCat === category.id ? "text-[#FF6347]" : "text-white"
+                    }`}
+                  >
+                    {category.name}
+                  </Text>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+            <FlatList
+            data={
+              FOOD_DATA.filter((item) =>
+                activeCat === 1 ? true : item.cat === activeCat
+              )
             }
+            keyExtractor={(item) => item.id.toString()}
+            numColumns={2}
+            columnWrapperStyle={{ justifyContent: "space-between" }}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 120 }}
+            renderItem={({ item }) => (
+              <FoodCardMini item={item} onOpen={() => openOrder(item)} />
+            )}
           />
+
+
+          </View>
+          
         </View>
       </ImageBackground>
     </KeyboardAvoidingView>
-  );
+  )
 }
+
+export default home
