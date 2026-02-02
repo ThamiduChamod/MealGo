525bd3c62b553b21a8dc2a27cd6755f7a4957b3a
ThamiduChamod <thamiduchamod748@gmail.com>
2026-02-02 17:28:03 +0530

    home card data loading

--- app/(dashboard)/home.tsx
+++ app/(dashboard)/home.tsx
@@ -1,18 +1,52 @@
 import AutoScrollBanner from '@/components/bannerComponent'
 import FoodCardMini from '@/components/foodCardComponents'
-import { FOOD_DATA } from '@/constants/data'
+import { auth } from '@/services/firebase'
+import { getAll } from '@/services/homeService'
 import { Ionicons } from '@expo/vector-icons'
 import { useRouter } from 'expo-router'
-import React, { useState } from 'react'
-import { View, Text, ImageBackground, StyleSheet, KeyboardAvoidingView, Platform, StatusBar, TouchableOpacity, FlatList } from 'react-native'
+import { onAuthStateChanged } from 'firebase/auth'
+import React, { useEffect, useState } from 'react'
+import { View, Text, ImageBackground, StyleSheet, KeyboardAvoidingView, Platform, StatusBar, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 
 const home = () => {
   const [activeCat, setActiveCat] = useState(1);
+  const [foodData, setFoodData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
+  
+  useEffect(() => {
+  // 1. User ඉන්නවාද කියලා බලන Listener එක
+  const unsubscribe = onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // ✅ User ඉන්නවා නම් විතරක් data fetch කරන්න
+      console.log("User logged in, fetching data...");
+      loadFoodData(); 
+    } else {
+      // ❌ User නැත්නම් Login එකට යවන්න (optional)
+      console.log("No user found");
+      // router.replace('/login'); 
+    }
+  });
+
+  const loadFoodData = async () => {
+    try {
+      setLoading(true)
+      const data = await getAll();
+      if (data == null) throw new Error("No user logged in");
+      setFoodData(data);
+    } catch (err) {
+      setLoading(false)
+      console.error(err);
+    }finally {setLoading(false)}
+  };
+
+  return () => unsubscribe(); // Cleanup the listener
+}, []);
+
 
   const openOrder = (item: any) => {
-    router.push(`/details/${item.id}`);
+    router.push(`/details/${item.id}?id=${item.id}`);
   };
 
 
@@ -83,7 +117,7 @@ const home = () => {
             </ScrollView>
             <FlatList
             data={
-              FOOD_DATA.filter((item) =>
+              foodData.filter((item) =>
                 activeCat === 1 ? true : item.cat === activeCat
               )
             }
